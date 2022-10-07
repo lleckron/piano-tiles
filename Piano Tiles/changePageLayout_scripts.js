3e0a4f230d293a21fe6b6b78fd4d8ff4d5fb78c8
lleckron <leckronluke@gmail.com>
2022-10-07 05:33:29 -0400

    add function to add selection btns & modify styles

--- Piano Tiles/changePageLayout_scripts.js
+++ Piano Tiles/changePageLayout_scripts.js
@@ -1,8 +1,31 @@
-/*function playButtonClick() {
-    
-}*/
+function playButtonClick() {
+    removeTitleDivs();
+    addSelectionDiv();
+    addSelectionButtons();
+}
 
-function removeDivs() {
+function removeTitleDivs() {
     let header = document.getElementById("content-container");
     header.remove();
+}
+
+function addSelectionDiv() {
+    const selectionContainer = document.createElement("div");
+    selectionContainer.setAttribute("id","selection-container");
+    document.body.appendChild(selectionContainer);
+}
+
+function addSelectionButtons() {
+    const selectionContainer = document.getElementById("selection-container");
+    for(let i = 0; i < 3; i++) {
+        let newButtonDiv = document.createElement("div");
+        newButtonDiv.setAttribute("id","selection-button-div");
+        let newButton = document.createElement("button");
+        newButton.setAttribute("id","selection-button");
+        //newButton.setAttribute("onclick","");
+        newButton.textContent = "Test";
+        newButtonDiv.appendChild(newButton);
+        selectionContainer.appendChild(newButtonDiv);
+        document.body.appendChild(selectionContainer);
+    }
 }
